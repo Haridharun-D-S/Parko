963cd1ee4a0bdeef2fbec6ede3716f6582dcfccc
unknown <mrbrokenml@gmail.com>
2025-12-29 18:51:44 +0530

    Fix assets and rebuild UI

---
+++ Parko_UI/src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ]
+})
+export class AppModule {}
